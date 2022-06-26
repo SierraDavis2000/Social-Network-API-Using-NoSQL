84638d79944845aa5a57cb8ac64ed6a373c88b6b
SierraDavis2000 <99284604+SierraDavis2000@users.noreply.github.com>
2022-06-26 18:22:17 -0400

    Models created

--- Models/User.js
+++ Models/User.js
@@ -1,2 +1,42 @@
 const { Schema, model } = require('mongoose');
 
+const userSchema = new Schema (
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ]
+    },
+    {
+        toJSON: {getters: true, virtuals: true} , 
+        id: false
+    },
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
